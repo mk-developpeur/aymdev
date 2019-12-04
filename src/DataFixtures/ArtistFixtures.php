7464cfff6e5ccf95e6cef563c07906abf9ef8139
AymDev <aymericmayeux@gmail.com>
2019-12-04 16:00:52 +0100

    Ajout des Fixtures des Artists

---
+++ src/DataFixtures/ArtistFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Artist;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class ArtistFixtures extends BaseFixture
+{
+    protected function loadData(ObjectManager $manager)
+    {
+        // Générer 50 artistes
+        $this->createMany(50, function () {
+            // Construction du nom d'artiste
+            $name = $this->faker->randomElement(['DJ ', 'MC ', 'Lil ', '']);
+            $name .= $this->faker->firstName;
+            $name .= $this->faker->randomElement([
+                ' ' . $this->faker->realText(10),
+                ' aka ' . $this->faker->domainWord,
+                ' & The ' . $this->faker->lastName,
+                ''
+            ]);
+
+            // Instanciation de l'entité
+            $artist = (new Artist())
+                ->setName($name)
+                ->setDescription($this->faker->realText(50))
+            ;
+
+            // Retourner l'entité
+            return $artist;
+        });
+
+        // Enregistrer les entités en BDD
+        $manager->flush();
+    }
+}
