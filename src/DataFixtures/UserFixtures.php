5a416003a01ff1a1b8d7151c66931522cb77a426
AymDev <aymericmayeux@gmail.com>
2019-12-06 16:33:19 +0100

    Ajout du composant de sécurité

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace App\DataFixtures;
+
+
+use App\Entity\User;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends BaseFixture
+{
+    /** @var UserPasswordEncoderInterface */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    protected function loadData(ObjectManager $manager)
+    {
+        $this->createMany(10, 'user_admin', function ($num) {
+            $admin = new User();
+            $admin
+                ->setEmail(sprintf('admin%d@kritik.fr', $num))
+                ->setRoles(['ROLE_ADMIN'])
+                ->setPassword($this->encoder->encodePassword($admin, 'admin' . $num))
+            ;
+
+            return $admin;
+        });
+
+        $this->createMany(40, 'user_user', function ($num) {
+            $user = new User();
+            $user
+                ->setEmail(sprintf('user%d@mail.org', $num))
+                ->setPassword($this->encoder->encodePassword($user, 'user' . $num))
+            ;
+
+            return $user;
+        });
+
+        $manager->flush();
+    }
+}
