0114cc79c8c2545b438c380f2e5c7148d7bdf5f9
AymDev <aymericmayeux@gmail.com>
2019-12-09 10:23:52 +0100

    Ajout du pseudo aux utilisateurs

---
+++ src/Migrations/Version20191209083918.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191209083918 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user ADD pseudo VARCHAR(40) DEFAULT NULL');
+        $this->addSql('UPDATE user SET pseudo = CONCAT("user_", id)');
+        $this->addSql('ALTER TABLE user CHANGE pseudo pseudo VARCHAR(40) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D64986CC499D ON user (pseudo)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX UNIQ_8D93D64986CC499D ON user');
+        $this->addSql('ALTER TABLE user DROP pseudo');
+    }
+}
